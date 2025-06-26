d2d3c78191e600e60c6839312d0da9d22842bb99
pyschonoice <monukk2002@gmail.com>
2025-06-26 21:44:55 +0530

    added analytics

--- src/main.jsx
+++ src/main.jsx
@@ -6,9 +6,9 @@ import App from "./App";
 import "./index.css";
 import { ThemeProvider } from "./contexts/ThemeContext";
 import { AuthProvider } from "./contexts/AuthContext";
-
+import { inject } from '@vercel/analytics';
 const queryClient = new QueryClient();
-
+inject();
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider>
